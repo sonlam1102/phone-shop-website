63cabecdd33bc43326e6ef6ffd16d95606a19698
Thanh Son <son.luu@youthdev.net>
2018-10-09 09:42:45 +0700

    upload procedure

---
+++ app/Tools/Upload.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Tools;
+use Illuminate\Http\Request;
+
+class Upload {
+    public static function imageUploadProfile(Request $request, $user_id)
+    {
+        $request->validate([
+            'img' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        try {
+            $imageName = $user_id.'.'.$request->img->getClientOriginalExtension();
+            $request->img->move(public_path('image/avatar'), $imageName);
+            return '/image/avatar/'.$imageName;
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+}
