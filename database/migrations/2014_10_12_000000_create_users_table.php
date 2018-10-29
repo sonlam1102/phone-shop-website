280e603052a0716c2418c7bcf68ef416b237630b
Thanh Son <son.luu@youthdev.net>
2018-10-29 19:24:59 +0700

    update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->smallInteger('type')->default(2);
+            $table->unsignedInteger('active')->default(1);
         });
     }
 
