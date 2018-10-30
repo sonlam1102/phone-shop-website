711a7c3426b71872d4b573ae3befffabf00857f5
Thanh Son <son.luu@youthdev.net>
2018-10-30 21:46:17 +0700

    add model product

---
+++ database/migrations/2018_10_30_143337_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->unsignedInteger('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            $table->string('code')->nullable();
+            $table->string('name');
+            $table->string('price')->nullable();
+            $table->date('manufacture_date')->nullable();
+            $table->timestamps();
+            $table->unsignedInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('company')->onDelete('cascade');
+            $table->string('img')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
