50fdbb0fe4925470652c6d321ee81e0d96a20f2b
Thanh Son <son.luu@youthdev.net>
2018-10-31 09:26:41 +0700

    handle upload image

--- app/Tools/Upload.php
+++ app/Tools/Upload.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Tools;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class Upload {
     public static function imageUploadProfile($request, $user_id)
@@ -8,9 +9,15 @@ class Upload {
         if (!$request->img) {
             return null;
         }
+
+        if(!File::exists('/image/avatar/')) {
+            File::makeDirectory('/image/avatar/', $mode = 0777, true, true);
+        }
+
         $request->validate([
             'img' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
+
         try {
             $imageName = $user_id.'.'.$request->img->getClientOriginalExtension();
             $request->img->move(public_path('image/avatar'), $imageName);
@@ -22,12 +29,18 @@ class Upload {
 
     public static function productImageUpload($request, $product_name)
     {
-        if (!$request->product_img) {
+        if (!$request->file('product_img')) {
             return null;
         }
+
+        if(!File::exists('/image/product/')) {
+            File::makeDirectory('/image/product/', $mode = 0777, true, true);
+        }
+
         $request->validate([
             'img' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
+
         if (!$product_name) {
             $product_name = date("D M d, Y G:i");
         }
@@ -36,7 +49,7 @@ class Upload {
         }
 
         try {
-            $imageName = $product_name.'.'.$request->product_img->getClientOriginalExtension();
+            $imageName = $product_name.'.'.$request->file('product_img')->getClientOriginalExtension();
             $request->img->move(public_path('image/product'), $imageName);
             return '/image/product/'.$imageName;
         } catch (\Exception $e) {
