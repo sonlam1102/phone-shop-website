729c38c19b481cd920fafda3d814d31a0c44bdb8
Thanh Son <son.luu@youthdev.net>
2018-11-08 09:16:28 +0700

    update product UI main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -93,7 +93,7 @@
                                 <div class="thumbnail product-box">
                                     <img class="product_img" src="{{ $item->img ? $item->img : null }}"  onerror="this.src='/img/dummyimg.png';"/>
                                     <div class="caption">
-                                        <h3><a href="#"> {{ $item->name }}</a></h3>
+                                        <p><strong> {{ $item->name }}</strong></p>
                                         <p>Price : <strong>{{ $item->price }}</strong>  </p>
                                         <p>Brand : <strong>{{ $item->brand->name }}</strong>  </p>
                                         <p><a href="#" class="btn btn-success" role="button">Add To Cart</a> <a href="#" class="btn btn-primary" role="button">See Details</a></p>
