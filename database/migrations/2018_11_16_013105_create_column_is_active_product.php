12db9b9cdb3e7ca2d1520c46371cafd89d9a938a
Thanh Son <son.luu@youthdev.net>
2018-11-16 08:34:07 +0700

    update product migration

---
+++ database/migrations/2018_11_16_013105_create_column_is_active_product.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColumnIsActiveProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function(Blueprint $table) {
+            if (!Schema::hasColumn('products', 'is_active')) {
+                $table->boolean('is_active')->default(true);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function(Blueprint $table) {
+            if (Schema::hasColumn('products', 'is_active')) {
+                $table->dropColumn('is_active');
+            }
+        });
+    }
+}
